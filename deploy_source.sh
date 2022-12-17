c57fc6322b230cf13e33ea1910ec7b0fa3365f22
huweihuang <huweihuang@foxmail.com>
2022-12-17 16:16:33 +0800

    use blog-source to deploy

---
+++ deploy_source.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+set -e
+
+MESSAGE=$1
+
+# update blog in blog-source repo
+BLOG_SOURCE_DIR="../blog-source/linux-notes"
+MASTER_DIR="$(pwd)"
+GHPAGE_DIR="${MASTER_DIR}/gh-pages"
+
+# clean and copy blog
+rm -fr $(ls | grep -v -E 'node_modules|book.json|README.md|SUMMARY.md|.gitignore|LICENSE|gh-pages|deploy.sh|deploy_source.sh')
+cp -fr ${BLOG_SOURCE_DIR}/* ./
+
+# clone gh-pages
+rm -fr gh-pages
+git clone -b gh-pages https://github.com/huweihuang/python-notes.git gh-pages
+
+# build
+gitbook build
+
+# clean GHPAGE_DIR
+rm -fr ${GHPAGE_DIR}/*
+
+# copy _book to GHPAGE_DIR
+cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
+
+# git commit
+cd ${GHPAGE_DIR}
+git add --all
+git commit -m "${MESSAGE}"
+git push origin gh-pages
